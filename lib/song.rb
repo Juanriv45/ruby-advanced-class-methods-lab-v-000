4741c49bec070f3feeb0b6638968e529558a4d3d
Juan Rivera <juanriv45@gmail.com>
2018-02-21 05:27:17 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,4 +31,10 @@ class Song
       song.name == song_name
     end
   end
+  def self.find_or_create_by_name(song_name)
+    self.all.detect do |song|
+      song.name == song_name
+    end
+  end
+  
 end
